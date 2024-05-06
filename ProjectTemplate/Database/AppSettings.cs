2ec304091e3d26b251b93f4bd2552fe04a642c56
Vinicius Dias <diasvinicius95@outlook.com>
2024-05-06 11:57:33 -0300

    adicionando metodos de database

---
+++ ProjectTemplate/Database/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ProjectTemplate.Database
+{
+    internal class AppSettings
+    {
+        public string ReadConnectionString(string conectionString)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath($@"{AppDomain.CurrentDomain.BaseDirectory}")
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var conn = configuration.GetConnectionString(conectionString);
+            return conn;
+        }
+
+    }
+}
---
+++ ProjectTemplate/Database/Conection.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjectTemplate.Database
+{
+    internal class Conection
+    {
+        public SqlConnection conn = new SqlConnection();
+        public AppSettings appSettings = new AppSettings();
+        //Exception exception = new Exception();
+
+        public Conection()
+        {
+            conn.ConnectionString = appSettings.ReadConnectionString("Database");
+        }
+
+        public SqlConnection ConectaComOBanco()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        public void DesconectaComOBanco()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+        public void ExecuteQuerySemRetorno(string query)
+        {
+            ConectaComOBanco();
+            SqlCommand cmd = conn.CreateCommand();
+            SqlTransaction transaction;
+            transaction = conn.BeginTransaction("ConsultasSemRetorno");
+            cmd.Connection = conn;
+            cmd.Transaction = transaction;
+            cmd.CommandTimeout = 120;
+            cmd.CommandText = query;
+
+            cmd.ExecuteNonQuery();
+
+            transaction.Commit();
+
+            DesconectaComOBanco();
+        }
+
+        public DataSet Select(string query)
+        {
+            DataSet ds = new DataSet();
+            ConectaComOBanco();
+            SqlCommand cmd = conn.CreateCommand();
+            SqlTransaction transaction;
+            transaction = conn.BeginTransaction("Consulta");
+            cmd.Connection = conn;
+            cmd.Transaction = transaction;
+            cmd.CommandTimeout = 120;
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+
+            da.Fill(ds);
+            transaction.Commit();
+            DesconectaComOBanco();
+            return ds;
+        }
+
+        public void Bulky(DataTable dt, string destinyTable, int columMapping = 0)
+        {
+            ConectaComOBanco();
+            using (SqlTransaction transaction = conn.BeginTransaction())
+            {
+                using (SqlBulkCopy bulkCopy = new SqlBulkCopy(conn, SqlBulkCopyOptions.KeepIdentity, transaction))
+                {
+                    try
+                    {
+                        bulkCopy.DestinationTableName = destinyTable;
+                        int cont = 0;
+                        foreach (DataColumn column in dt.Columns)
+                        {
+                            SqlBulkCopyColumnMapping map = new SqlBulkCopyColumnMapping(column.ColumnName.ToString(), cont + columMapping);
+                            bulkCopy.ColumnMappings.Add(map);
+                            cont++;
+                        }
+                        bulkCopy.WriteToServer(dt);
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Erro");
+                        Console.WriteLine(ex.Message);
+                        //Console.ReadLine();
+
+                        transaction.Rollback();
+                        DesconectaComOBanco();
+                    }
+                    finally
+                    {
+                        DesconectaComOBanco();
+                    }
+                }
+            }
+        }
+    }
+}
